62421e49d584aed5774cdac1fcab6ee0d982df67
was2309 <vasilijem92@gmail.com>
2022-05-25 17:11:53 +0200

    Added trim to search employee part

--- viewmodel/employeelist/EmployeeListVM.php
+++ viewmodel/employeelist/EmployeeListVM.php
@@ -17,7 +17,7 @@ class EmployeeListVM
         $employee_data = null;
 
         if(isset($_REQUEST['page']) && isset($_REQUEST['s'])){
-            $name = esc_html($_REQUEST['s']);
+            $name = trim(esc_html($_REQUEST['s']));
 
             $employee_data = $this->employee_service->find_employee_by_last_name($name);
         }else{
